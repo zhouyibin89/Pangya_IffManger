88ff5084bd4c04950019c5fe8c5822faa867c8fd
zhouyibin89 <zhouyibin1989@gmail.com>
2017-07-16 01:29:31 +0800

    Add files via upload

--- Pangya_IffManger/Part.cs
+++ Pangya_IffManger/Part.cs
@@ -19,28 +19,51 @@ namespace Pangya_IffManger
     /// </summary>
     public class Part
     {
-        public bool Enabled { get; set; }
-
-        public int Id { get; set; }
-        //public int Id2 { get; set; }
-
-        public string Name { get; set; }
-
-        public int Price { get; set; }
-
-        public int PriceType { get; set; }
-
+        public bool Enabled { get; set; }//4
+        public int Id { get; set; } //public int Id2 { get; set; }  4
+        public string Name { get; set; }//8
+        public byte level { get; set; }//48
+        public string icon { get; set; }//49
+        public int falg { get; set; }//89
+        public int Price { get; set; }//92
+        public int desconto { get; set; }//96
+        public int unknown { get; set; }//
+        public int PriceType { get; set; }//104
+        public int qnt_per_tiki_pts { get; set; }//
+        public int tiki_points { get; set; }//
+        public int milage_pts { get; set; }//
+        public int bonus_prob { get; set; }//
+        public int milage_pts2 { get; set; }//
+        public int milage_pts3 { get; set; }//
+        public int tipo_tiki_shop { get; set; }//
+        public int tiki_pang { get; set; }//
+        public int active_date { get; set; }//
+        public string date_inicio { get; set; }// <--- how to change value to DateTime?
+        public string date_fim { get; set; }//<--- how to change value to DateTime?
+        public string mpet { get; set; }//
+        public int ucc_type { get; set; }//
+        public int part_type { get; set; }//
+        public int unknown2 { get; set; }//
+        public string textura_1 { get; set; }//
+        public string textura_2 { get; set; }//
+        public string textura_3 { get; set; }//
+        public string textura_4 { get; set; }//
+        public string textura_5 { get; set; }//
+        public string textura_6 { get; set; }//
         public int Power { get; set; } //Index 460
         public int Control { get; set; } //Index 462
         public int Accuracy { get; set; } //Index 464
         public int Spin { get; set; } //Index 466
         public int Curve { get; set; } //Index 468
-
         public int PowerSlot { get; set; } //Index 470
         public int ControlSlot { get; set; } //Index 472
         public int AccuracySlot { get; set; } //Index 474
         public int SpinSlot { get; set; } //Index 476
-        public int CurveSlot { get; set; } //Index 478
-
+        public int CurveSlot { get; set; } //Index 47
+        public string blank { get; set; }
+        public int unknown3 { get; set; }
+        public int unknown4 { get; set; }
+        public int valor_rental { get; set; }
+        public int unknown5 { get; set; }
     }
 }
--- Pangya_IffManger/PartManager.cs
+++ Pangya_IffManger/PartManager.cs
@@ -49,9 +49,35 @@ namespace Pangya_IffManger
                 Enabled = Convert.ToBoolean(data[0]), //Posição 0
                 Id = data.ToIntenger(startIndex: 4, count: 4), //Posição 4
                 Name = Encoding.ASCII.GetString(data.Skip(8).TakeWhile(x => x != 0x00).ToArray()), //Posição 8, lê o nome enquanto o byte for diferente de 0x00
-                Price = data.ToIntenger(startIndex: 92, count: 2), //Posição 92
-                PriceType = Convert.ToInt32(data[104]), //Posição 104
+                level = Convert.ToByte(data[48]),
+                icon = Encoding.ASCII.GetString(data.Skip(49).TakeWhile(x => x != 0x00).ToArray()), //Posição 8, lê o nome enquanto o byte for diferente de 0x00
 
+                falg = Convert.ToInt32(data[89]), //Posição 104
+                Price = data.ToIntenger(startIndex: 92, count: 2), //Posição 92
+                desconto = data.ToIntenger(startIndex: 96, count: 2), //Posição 96
+                unknown = Convert.ToInt32(data[100]), //Posição 100
+                PriceType = Convert.ToInt32(data[104]), //Posição 104
+                qnt_per_tiki_pts = Convert.ToInt32(data[108]), //Posição 108
+                tiki_points = Convert.ToInt32(data[112]), //Posição 104
+                milage_pts = Convert.ToInt16(data[116]), //Posição 104
+                bonus_prob = Convert.ToInt16(data[118]), //Posição 104
+                milage_pts2 = Convert.ToInt16(data[120]), //Posição 104
+                milage_pts3 = Convert.ToInt16(data[122]), //Posição 104
+                tipo_tiki_shop = Convert.ToInt32(data[124]), //Posição 104
+                tiki_pang = Convert.ToInt32(data[128]), //Posição 104
+                active_date = Convert.ToInt32(data[132]), //Posição 104
+                date_inicio = Encoding.ASCII.GetString(data.Skip(136).TakeWhile(x => x != 0x00).ToArray()), //<--- how to change value to DateTime?
+                date_fim = Encoding.ASCII.GetString(data.Skip(152).TakeWhile(x => x != 0x00).ToArray()), //
+                mpet = Encoding.ASCII.GetString(data.Skip(168).TakeWhile(x => x != 0x00).ToArray()), //Posição 104
+                ucc_type = Convert.ToInt32(data[208]), //Posição 104
+                part_type = Convert.ToInt32(data[212]), //Posição 104
+                unknown2 = Convert.ToInt32(data[216]), //Posição 104
+                textura_1 = Encoding.ASCII.GetString(data.Skip(220).TakeWhile(x => x != 0x00).ToArray()), //Posição 104
+                textura_2 = Encoding.ASCII.GetString(data.Skip(260).TakeWhile(x => x != 0x00).ToArray()), //Posição 104
+                textura_3 = Encoding.ASCII.GetString(data.Skip(300).TakeWhile(x => x != 0x00).ToArray()), //Posição 104
+                textura_4 = Encoding.ASCII.GetString(data.Skip(340).TakeWhile(x => x != 0x00).ToArray()), //Posição 104
+                textura_5 = Encoding.ASCII.GetString(data.Skip(380).TakeWhile(x => x != 0x00).ToArray()), //Posição 104
+                textura_6 = Encoding.ASCII.GetString(data.Skip(420).TakeWhile(x => x != 0x00).ToArray()), //Posição 104
                 //Attributes
                 Power = Convert.ToInt32(data[460]), //Posição 460
                 Control = Convert.ToInt32(data[462]),
@@ -64,7 +90,12 @@ namespace Pangya_IffManger
                 ControlSlot = Convert.ToInt32(data[472]),
                 AccuracySlot = Convert.ToInt32(data[474]),
                 SpinSlot = Convert.ToInt32(data[476]),
-                CurveSlot = Convert.ToInt32(data[478])
+                CurveSlot = Convert.ToInt32(data[478]),
+                blank = Encoding.ASCII.GetString(data.Skip(480).TakeWhile(x => x != 0x00).ToArray()), //Posição 104
+                unknown3 = Convert.ToInt32(data[528]), //Posição 104
+                unknown4 = Convert.ToInt32(data[532]), //Posição 104
+                valor_rental = Convert.ToInt32(data[536]), //Posição 104
+                unknown5 = Convert.ToInt32(data[540])
             };
         }
     }
